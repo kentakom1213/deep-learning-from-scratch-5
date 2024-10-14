216bd81f75c6fdbbf428a4456b76ac8f40f4c040
kentakom1213 <kentakom1213@gmail.com>
2024-10-15 00:42:59 +0900

    [add] step4（途中まで）

--- myml/src/normal/mod.rs
+++ myml/src/normal/mod.rs
@@ -1,5 +1,5 @@
 mod normal_multi;
 mod normal_single;
 
-pub use normal_multi::multivariate_normal;
+pub use normal_multi::{multivariate_normal, multivariate_normal_sample};
 pub use normal_single::normal;
